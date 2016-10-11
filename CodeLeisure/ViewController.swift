bfa3dac78e53a20660ed1d557fda73b2961a736b
千锋 <qianfeng@joke.local>
2016-10-11 19:43:31 +0800

    first

---
+++ CodeLeisure/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CodeLeisure
+//
+//  Created by 千锋 on 16/10/11.
+//  Copyright © 2016年 Mr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'CodeLeisure' do
+  # Comment this line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+
+pod 'SnapKit', '~>0.0'
+  # Pods for CodeLeisure
+
+end
